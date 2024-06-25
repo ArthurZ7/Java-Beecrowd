1c9ba8dff243a396bd72f1ff7a729c8aec9b4f2a
Arthur I Borges <arthuriborges100@gmail.com>
2024-06-25 19:33:48 -0300

    atualizando diretorios

---
+++ BEE-1003_Soma_Simples.java
@@ -0,0 +1,20 @@
+/*---------------------*
+| ArthurZ7             |
+| BEE 1003             |
+| Soma Simples         |
+*---------------------*/
+import java.util.Scanner;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner s = new Scanner (System.in);
+
+    int A = s.nextInt();
+    int B = s.nextInt();
+        
+    System.out.println("SOMA = " + (A+B));
+    s.close ();
+  }
+}
+
---
+++ BEE-1005_Media_1.java
@@ -0,0 +1,22 @@
+/*---------------------*
+| ArthurZ7             |
+| BEE 1005             |
+| Média 1              |
+*---------------------*/
+import java.util.Scanner;
+public class Main
+{
+  public static void main (String[]args)
+  {
+    Scanner s = new Scanner (System.in);
+
+    double A = s.nextDouble()*3.5;
+    double B = s.nextDouble()*7.5;
+    double MEDIA = (A+B)/11;
+        
+    System.out.printf("MEDIA = %.5f", MEDIA);
+    System.out.println();
+    s.close ();
+  }
+}
+
