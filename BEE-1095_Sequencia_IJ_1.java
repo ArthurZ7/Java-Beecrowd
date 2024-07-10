d6253d59732850dc74c86accbc1f41704dd94e6c
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-07-10 20:45:43 -0300

    add BEE-1095

---
+++ BEE-1095_Sequencia_IJ_1.java
@@ -0,0 +1,18 @@
+/*----------------------*
+| ArthurZ7             |
+| BEE 1095             |
+| Sequencia IJ 1       |
+*----------------------*/
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String[]args)
+    {
+    
+    int i=1;
+    for (int j=60; j>=0; j=j- 5){
+        System.out.printf("I=%d J=%d\n", i, j);
+        i= i+ 3;
+    }
+    }
+}
