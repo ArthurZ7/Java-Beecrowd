100c7545c3d5df3b64efb53b0d73e9d147a7c7b3
Arthur Borges <103152194+ArthurZ7@users.noreply.github.com>
2024-08-13 10:18:48 -0300

    BEE 1171
    beecrowd | 1171
    Frequência de Números
    Adaptado por Neilor Tonin, URI  Brasil
    
    Timelimit: 1
    Neste problema sua tarefa será ler vários números e em seguida dizer quantas vezes cada número aparece na entrada de dados, ou seja, deve-se escrever cada um dos valores distintos que aparecem na entrada por ordem crescente de valor.
    
    Entrada
    A entrada contém apenas 1 caso de teste. A primeira linha de entrada contem um único inteiro N, que indica a quantidade de valores que serão lidos para X (1 ≤ X ≤ 2000) logo em seguida. Com certeza cada número não aparecerá mais do que 20 vezes na entrada de dados.
    
    Saída
    Imprima a saída de acordo com o exemplo fornecido abaixo, indicando quantas vezes cada um deles aparece na entrada por ordem crescente de valor.

--- BEE-1171_Frequencia_de_Numeros.java
+++ BEE-1171_Frequencia_de_Numeros.java
@@ -12,19 +12,19 @@ public class Main
         
 	int n = s.nextInt();
 	int num[] = new int[n];
-	int posição[] = new int[2000];
+	int posicao[] = new int[2000];
 	
 	for (int i=0; i<n ; i++) {
 	    num[i] = s.nextInt();
 
 	    // numero inserido == posição 
 	    // incrementa na posição relativa ao numero inserido
-        posição[num[i] - 1]++;
+        posicao[num[i] - 1]++;
     }
     
     for (int i=0; i<2000; i++) {
-        if (posição[i] > 0){
-            System.out.printf("%d aparece %d vez(es)\n", i+ 1, posição[i]);
+        if (posicao[i] > 0){
+            System.out.printf("%d aparece %d vez(es)\n", i+ 1, posicao[i]);
         }
     }
     }
